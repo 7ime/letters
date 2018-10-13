54a0be01a578f57fb347f8845b1ff5bcb6d0e252
Ilya Lopatin <lopatsin1990@gmail.com>
2018-10-13 21:48:53 +0300

    [FE] create build

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,52 @@
 'use strict';
 
-const gulp = require('gulp');
-const hb = require('gulp-hb');
-const rename = require('gulp-rename');
-const through = require('through2');
-const path = require('path');
-const runSequence = require('run-sequence');
+const gulp              = require('gulp');
+const hb                = require('gulp-hb');
+const through           = require('through2');
+const path              = require('path');
+const runSequence       = require('run-sequence');
 
-const inlineCss = require('gulp-inline-css');
+const browserSync       = require('browser-sync');
+const sass              = require('gulp-sass');
+const inlineCss         = require('gulp-inline-css');
 
-var path_to_dist = "dest/";
-var path_to_templates = "src/html";
+const notify            = require('gulp-notify');
+const rename            = require('gulp-rename');
+const concat            = require('gulp-concat');
+
+let path_to_templates = "src/html";
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: 'dest'
+        },
+        notify: false,
+    });
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/sass/**/*.+(scss|sass)')
+        .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest('src/html/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
 
 //сборка шаблонов
 gulp.task('handlebars', (done) => {
-
     // JSON файл общий для всех проектов
     gulp.src('src/main.json')
         .pipe(through.obj(function(file, enc, cb) {
-
             // Парсим и данные заносим в переменную commonData
             let commonData = JSON.parse(file.contents.toString());
-
             // JSON файл с данными по каждому шаблону
             gulp.src('src/json/*.json')
                 .pipe(through.obj(function(file, enc, cb) {
-
                     // Парсим и данные заносим в переменную templateData
                     let templateData = JSON.parse(file.contents.toString());
-
                     // Соотносим шаблон handlebars с JSON файлом
                     return gulp.src('src/templates/' + path.basename(file.path, '.json') + '.handlebars')
-
                     // Подставляем в него данные из всех JSON и partials
                         .pipe(hb()
                             .partials('src/partials/*.handlebars')
@@ -46,29 +61,54 @@ gulp.task('handlebars', (done) => {
                         .on('end', cb);
                 }));
         }));
-
     done();
 });
 
-gulp.task('inlineCss', (done) => {
-    return gulp.src('src/html/*.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('dest/'));
-    done();
+gulp.task('inlineCss', function() {
+    setTimeout(function() {
+        return gulp.src('src/html/*.html')
+            .pipe(inlineCss())
+            .pipe(gulp.dest('dest/'))
+            .pipe(browserSync.reload({stream: true}));
+    }, 500)
 });
 
-// gulp.task('build', function(done) {
-//     runSequence(
-//         'handlebars',
-//         // 'inlineCss',
-//         done
-//     );
-// });
+gulp.task('watchHandlebars', function(done) {
+    runSequence(
+        'handlebars',
+        'inlineCss',
+        done
+    );
+});
 
-gulp.task('build', gulp.series('handlebars', 'inlineCss', function (done) {
-    done();
-}));
+gulp.task('watchCss', function(done) {
+    runSequence(
+        'sass',
+        'inlineCss',
+        done
+    );
+});
 
 gulp.task('watch', () => {
-    gulp.watch('src/*.handlebars', gulp.parallel('handlebars'));
+    gulp.watch('src/**/*.handlebars', ['watchHandlebars']);
+    gulp.watch('src/sass/**/*.scss', ['watchCss']);
 });
+
+gulp.task('build', function(done) {
+    runSequence(
+        'sass',
+        'handlebars',
+        'inlineCss',
+        'browserSync',
+        done
+    );
+});
+
+gulp.task('dev', function(done) {
+    runSequence(
+        'build',
+        'watch',
+        done
+    );
+});
+
