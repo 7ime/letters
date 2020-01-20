5af276e39bb216e7a7c98646c88c737bcd0f8780
ilya.lopatin.by <ilya.lopatin@zensoft.io>
2020-01-20 20:07:55 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -14,101 +14,110 @@ const notify            = require('gulp-notify');
 const rename            = require('gulp-rename');
 const concat            = require('gulp-concat');
 
-let path_to_templates = "src/html";
+const fs                = require('fs');
+const replace           = require('gulp-replace');
+
+
+const dest = "dest/emails";
+
+const config = {
+  destImg: dest + '/img',
+};
 
 gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: 'dest'
-        },
-        notify: false,
-    });
+  browserSync({
+    server: {
+      baseDir: dest
+    },
+    notify: false,
+  });
 });
 
 gulp.task('sass', function() {
-    return gulp.src('src/sass/**/*.+(scss|sass)')
-        .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
-        .pipe(rename('main.css'))
-        .pipe(gulp.dest('src/html/css'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('src/sass/global.scss')
+    .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest('src/templates/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 //сборка шаблонов
 gulp.task('handlebars', (done) => {
-    // JSON файл общий для всех проектов
-    gulp.src('src/main.json')
+  // JSON файл общий для всех проектов
+  gulp.src('src/main.json')
+    .pipe(through.obj(function(file, enc, cb) {
+      // Парсим и данные заносим в переменную commonData
+      let commonData = JSON.parse(file.contents.toString());
+      // JSON файл с данными по каждому шаблону
+      gulp.src('src/json/*.json')
         .pipe(through.obj(function(file, enc, cb) {
-            // Парсим и данные заносим в переменную commonData
-            let commonData = JSON.parse(file.contents.toString());
-            // JSON файл с данными по каждому шаблону
-            gulp.src('src/json/*.json')
-                .pipe(through.obj(function(file, enc, cb) {
-                    // Парсим и данные заносим в переменную templateData
-                    let templateData = JSON.parse(file.contents.toString());
-                    // Соотносим шаблон handlebars с JSON файлом
-                    return gulp.src('src/templates/' + path.basename(file.path, '.json') + '.handlebars')
-                    // Подставляем в него данные из всех JSON и partials
-                        .pipe(hb()
-                            .partials('src/partials/*.handlebars')
-                            .data(commonData)
-                            .data(templateData)
-                        )
-                        // Переименовываем
-                        .pipe(rename({extname: ".html"}))
-                        // Отправляем в папку с шаблонами
-                        .pipe(gulp.dest(path_to_templates))
-                        .on('error', cb)
-                        .on('end', cb);
-                }));
+          // Парсим и данные заносим в переменную templateData
+          let templateData = JSON.parse(file.contents.toString());
+          // Map the handlebars template to a JSON file
+          return gulp.src('src/templates/' + path.basename(file.path, '.json') + '.handlebars')
+            .pipe(hb()
+              .partials('src/partials/*.handlebars')
+              .data(commonData)
+              .data(templateData)
+            )
+            .pipe(rename({extname: ".html"}))
+            .pipe(inlineCss({
+              removeStyleTags: true,
+              removeHtmlSelectors: true
+            }))
+            .pipe(replace(/<!--HEAD([\s\S]*?)HEAD-->/g, function(match, inner) {
+              return inner;
+            }))
+            .pipe(gulp.dest(dest))
+            .on('error', cb)
+            .on('end', cb);
         }));
-    done();
+    }));
+  done();
 });
 
-gulp.task('inlineCss', function() {
-    setTimeout(function() {
-        return gulp.src('src/html/*.html')
-            .pipe(inlineCss())
-            .pipe(gulp.dest('dest/'))
-            .pipe(browserSync.reload({stream: true}));
-    }, 500)
+gulp.task('img', function() {
+  return gulp.src('src/img/**')
+    .pipe(gulp.dest(config.destImg))
 });
 
 gulp.task('watchHandlebars', function(done) {
-    runSequence(
-        'handlebars',
-        'inlineCss',
-        done
-    );
+  runSequence(
+    'handlebars',
+    done
+  );
 });
 
 gulp.task('watchCss', function(done) {
-    runSequence(
-        'sass',
-        'inlineCss',
-        done
-    );
+  runSequence(
+    'sass',
+    'handlebars',
+    done
+  );
 });
 
 gulp.task('watch', () => {
-    gulp.watch('src/**/*.handlebars', ['watchHandlebars']);
-    gulp.watch('src/sass/**/*.scss', ['watchCss']);
+  gulp.watch('src/**/*.json', ['watchHandlebars']);
+  gulp.watch('src/**/*.handlebars', ['watchHandlebars']);
+  gulp.watch('src/sass/**/*.scss', ['watchCss']);
 });
 
 gulp.task('build', function(done) {
-    runSequence(
-        'sass',
-        'handlebars',
-        'inlineCss',
-        'browserSync',
-        done
-    );
+  runSequence(
+    'img',
+    'sass',
+    'handlebars',
+    done
+  );
 });
 
 gulp.task('dev', function(done) {
-    runSequence(
-        'build',
-        'watch',
-        done
-    );
+  runSequence(
+    'img',
+    'sass',
+    'handlebars',
+    'browserSync',
+    'watch',
+    done
+  );
 });
-
